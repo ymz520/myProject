93868f246de842f3e6d5e8bb1c3aaeb94d693578
ymz520 <biti@bitideMacBook-Pro.local>
2016-05-26 21:11:07 +0800

    qq
    aa

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProject
+//
+//  Created by biti on 16/5/26.
+//  Copyright © 2016年 biti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
